4e477b784d9c2fc53ef0305489c618b914206098
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-05-20 17:30:10 +0100

    Get pieces from torrent file

--- cmd/mybittorrent/torrent-parser.go
+++ cmd/mybittorrent/torrent-parser.go
@@ -7,9 +7,22 @@ import (
 	"os"
 )
 
+type Torrent struct {
+	Announce string
+	Info     TorrentInfo `bencode:"info"`
+}
+
+type TorrentInfo struct {
+	Length      int    `bencode:"length"`
+	Name        string `bencode:"name"`
+	PieceLength int    `bencode:"piece length"`
+	Pieces      string `bencode:"pieces"`
+}
+
 func parseTorrentFile(torrentFilePath string, startIndex int) {
 
 	torrentInfo, err := os.ReadFile(torrentFilePath)
+	var torrent Torrent
 
 	if err != nil {
 		fmt.Println(err)
@@ -31,22 +44,33 @@ func parseTorrentFile(torrentFilePath string, startIndex int) {
 		return
 	}
 
-	announce := torrentDict["announce"].(string)
-	infoDict := torrentDict["info"].(map[string]interface{})
-	fileLength := infoDict["length"].(int)
-
-	fmt.Println("Tracker URL:", announce)
-	fmt.Println("File Length:", fileLength)
+	torrent.Announce = torrentDict["announce"].(string)
+	torrent.Info = TorrentInfo{
+		Length:      torrentDict["info"].(map[string]interface{})["length"].(int),
+		Name:        torrentDict["info"].(map[string]interface{})["name"].(string),
+		PieceLength: torrentDict["info"].(map[string]interface{})["piece length"].(int),
+		Pieces:      torrentDict["info"].(map[string]interface{})["pieces"].(string),
+	}
 
 	hash := sha1.New()
 
-	if err := bencode.Marshal(hash, infoDict); err != nil {
+	if err := bencode.Marshal(hash, torrent.Info); err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	infoHash := hash.Sum(nil)
 
+	fmt.Printf("Tracker URL: %s\n", torrent.Announce)
+	fmt.Printf("Length: %d\n", torrent.Info.Length)
+
 	fmt.Printf("Info Hash: %x\n", infoHash)
+	fmt.Printf("Piece Length: %d\n", torrent.Info.PieceLength)
+
+	fmt.Printf("Piece Hashes: \n")
+
+	for i := 0; i < len(torrent.Info.Pieces); i += 20 {
+		fmt.Printf("%x\n", torrent.Info.Pieces[i:i+20])
+	}
 
 }
