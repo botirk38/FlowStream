5c1b792d079e4d1783e24e611d0fb81c46e15b69
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-05-20 15:55:36 +0100

    Implement functionality to parse single torrent file

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	"os"
 	"fmt"
+	"os"
 	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
@@ -11,17 +11,17 @@ import (
 // - 5:hello -> hello
 // - 10:hello12345 -> hello12345
 
-
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 
 	command := os.Args[1]
 	startIndex := 0
 
-	if command == "decode" {
+	switch command {
+	case "decode":
 		bencodedValue := os.Args[2]
 
-		decoded, err := Decode(bencodedValue, &startIndex) 
+		decoded, err := Decode(bencodedValue, &startIndex)
 		if err != nil {
 			fmt.Println(err)
 			return
@@ -29,8 +29,45 @@ func main() {
 
 		jsonOutput, _ := json.Marshal(decoded)
 		fmt.Println(string(jsonOutput))
-	} else {
-		fmt.Println("Unknown command: " + command)
-		os.Exit(1)
+
+	case "info":
+		torrentFile := os.Args[2]
+
+		torrentInfo, err := os.ReadFile(torrentFile)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		decoded, err := Decode(string(torrentInfo), &startIndex)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+
+		}
+
+		torrentDict, ok := decoded.(map[string]interface{})
+
+		if !ok {
+			fmt.Println("Error: Decoded value is not a dictionary")
+			return
+		}
+
+		announce := torrentDict["announce"].(string)
+		infoDict := torrentDict["info"].(map[string]interface{})
+		fileLength := infoDict["length"].(int)
+
+		fmt.Println("Tracker URL:", announce)
+		fmt.Println("File Length:", fileLength)
+
+	default:
+		{
+			fmt.Println("Invalid command")
+			os.Exit(1)
+		}
+
 	}
+
 }
